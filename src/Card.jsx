30d1f5247c7d05da76d9e2c5f2f62eae85ecf997
Jasmina ilya <islomovajasmina8@gmail.com>
2024-07-19 22:33:30 +0300

    second commit

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,12 +4,13 @@ import { useEffect, useState } from "react"
 
 function Card(){
     const [objData, setObjData] = useState([])
+    console.log(objData);
+
     useEffect(()=>{
         async function getData(){
             let response = await fetch ("https://random-data-api.com/api/v2/users")
             let data = await response.json()
             setObjData(data)
-            console.log(objData);
            
         }
     
@@ -19,13 +20,17 @@ function Card(){
 
     return(
         <>
+        {
+            console.log(objData.id)
+        }
         
         <div className="card">
             <div className="ava"></div>
-            <h1></h1>
-            <h2></h2>
-            <h2></h2>
-            <button></button>
+            {/* <img src=` {objData?.avatar} ` alt="" /> */}
+            <h2>Full name: <i>{objData?.first_name + " " + objData?.last_name}</i></h2>
+            <h2>Username: <i>{objData?.username}</i></h2>
+            <h2>Occupation: <i>{objData?.employment?.title}</i></h2>
+            {/* <button></button> */}
         </div>
         </>
     )
